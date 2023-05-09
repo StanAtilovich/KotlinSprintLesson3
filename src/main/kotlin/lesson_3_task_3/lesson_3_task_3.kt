06d5b55dcb279ebeef5af4ea1c3a2a63d18028bc
StanAtilovich <alano92@yandex.ru>
2023-05-09 12:44:23 +0300

    KS-3-3 исправил

--- src/main/kotlin/lesson_3_task_3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3_task_3/lesson_3_task_3.kt
@@ -4,9 +4,7 @@ fun main() {
     println("Введи число на которые вы хотите выучить таблицу умножения: ")
     val number = readLine()!!.toInt()
     println("Таблица умножения на число $number: ")
-
     for (i in 1 ..10 ){
         println("$number x $i = ${number * i}")
     }
-
 }
