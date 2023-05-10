8247d8ca01d5e95279d22ce468daef4772374d8d
StanAtilovich <alano92@yandex.ru>
2023-05-10 13:30:01 +0300

    KS_3_1

---
+++ src/main/kotlin/lesson_3_task_1/lesson_1_task_1.kt
@@ -0,0 +1,14 @@
+package lesson_3_task_1
+
+fun main(){
+    println("Введите время в формате 24 часов: ")
+    val userTime = readLine()!!.toInt()
+
+    if (userTime < DAY_TIME ){
+        println("Добрый день Пользователь")
+    }
+    if (userTime >= DAY_TIME){
+        println("Добрый вечер Пользователь")
+    }
+}
+const val DAY_TIME = 15
